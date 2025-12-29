79f35360a37ca42bdd7824a44a4e4f8f373b54a5
Sushant Singh <sushant22007@gmail.com>
2025-12-29 12:45:41 +0530

    Add routing with BrowserRouter and define routes for Home, Login, and Register

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import AppRoutes from './routes/AppRoutes';
 
 const App = () => {
   return (
     <div>
-      
+      <AppRoutes />
     </div>
   )
 }
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import{ BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const AppRoutes = () => {
   return (
     <div>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<div>Home</div>} />
+          <Route path="/login" element={<div>Login</div>} />
+          <Route path="/register" element={<div>Register</div>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
