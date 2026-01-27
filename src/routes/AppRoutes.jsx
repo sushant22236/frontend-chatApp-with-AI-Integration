f9d1f7943f63d5353e12fd5fe4d17b7052b93d5b
Sushant Singh <sushant22007@gmail.com>
2026-01-27 18:05:04 +0530

    Add UserAuth component for route protection in AppRoutes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -4,15 +4,16 @@ import Login from '../screen/Login.jsx';
 import Register from '../screen/Register.jsx';
 import Home from '../screen/Home.jsx';
 import Project from '../screen/Project.jsx';
+import UserAuth from '../auth/UserAuth.jsx';
 const AppRoutes = () => {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<UserAuth><Home /></UserAuth>} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/project" element={<Project />} />
+          <Route path="/project" element={<UserAuth><Project /></UserAuth>} />
         </Routes>
       </BrowserRouter>
     </div>
